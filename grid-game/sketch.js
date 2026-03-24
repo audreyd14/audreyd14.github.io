781d07edefcc9dd71d5494dbc6c0c5647d1a7c58
audreyd14 <ardeschamp@gmail.com>
2026-03-24 11:01:55 -0600

    started grid game

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -1,16 +1,62 @@
-// grid game- dots and boxes
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+const CELL_SIZE = 50;
+let grid;
+let rows;
+let cols;
+let liveCell;
+let deadCell;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rows = 2;
+  cols = 2;
+  grid = generateEmptyGrid(cols, rows);
+
 }
 
 function draw() {
   background(220);
+  strokeWeight(10);
+  liveCell = stroke(255, 0, 0);
+  deadCell = stroke(100, 100, 100);
+  displayGrid();
 }
 
+function displayGrid(){
+  for (let y = 0; y < rows; y++){
+    for (let x = 0; x < cols; x++){
+      line(x * CELL_SIZE, y * CELL_SIZE, x * CELL_SIZE, y *CELL_SIZE+CELL_SIZE);
+      line(x * CELL_SIZE, y * CELL_SIZE, x * CELL_SIZE+CELL_SIZE, y *CELL_SIZE);
+    }
+  }
+}
+
+
+function generateEmptyGrid(cols, rows){
+  let newGrid = [];
+
+  for(let y = 0; y < rows; y ++){
+    newGrid.push([]);
+    for(let x = 0; x < cols; x++){
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
+}
+
+function toggleCell(x, y) {
+  if (x >= 0 && x < cols && y >= 0 && y < rows) {
+    if (grid[y][x] === liveCell) {
+      grid[y][x] = deadCell;
+    }
+    else if (grid[y][x] === deadCell) {
+      grid[y][x] = liveCell;
+    }
+  }
+}
+
+function mousePressed(){
+  let x = mouseX;
+  let y = mouseY;
+
+  toggleCell(x,y);
+}

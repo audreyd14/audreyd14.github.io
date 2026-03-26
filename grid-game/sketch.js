46e46b34712353161692ded24ed736e6a83af1c9
audreyd14 <ardeschamp@gmail.com>
2026-03-26 11:03:22 -0600

    added line and rect grid and lines toggle

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -6,9 +6,12 @@
 // - describe what you did to take this project "above and beyond"
 
 const CELL_SIZE = 50;
+const LINE_LENGTH = 50;
 let grid;
+let lines;
 let rows;
 let cols;
+let lineWidth = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -21,14 +24,12 @@ function setup() {
 function draw() {
   background(220);
   displayGrid();
+  displayLines();
 }
 
 function displayGrid(){
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
-      if (grid[y][x] === 1){
-        fill("black");
-      }
       if (grid[y][x] === 0){
         fill("white");
       }
@@ -41,50 +42,104 @@ function displayGrid(){
 function displayLines(){
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
-      line(x * CELL_SIZE, y * CELL_SIZE, x * CELL_SIZE + CELL_SIZE, y * CELL_SIZE + CELL_SIZE);
+      if (lines[y][x] === 0){
+        stroke(200, 200, 200);
+        strokeWeight(lineWidth);
+        line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH);
+        line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
+      }
+      if (lines[y][x] === 1){
+        stroke(200, 0, 0);
+        strokeWeight(lineWidth);
+        line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH);
+      }
+      if (lines[y][x] === 2){
+        stroke(200, 0, 0);
+        strokeWeight(lineWidth);
+        line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH , y * LINE_LENGTH + LINE_LENGTH);
+      }
+
     }
   }
 }
 
-function generateLines(){
-  
-}
+
 
 function mousePressed(){
-  let x = Math.floor(mouseX/CELL_SIZE);
-  let y = Math.floor(mouseY/CELL_SIZE);
+  let x = Math.floor(mouseX/LINE_LENGTH);
+  let y = Math.floor(mouseY/LINE_LENGTH);
 
-  toggleGrid(x, y);
+  toggleLinesX(x, y);
+  toggleLinesY(x, y);
 }
 
-function toggleGrid(x, y){
+function toggleLinesX(x, y){
   if (x>=0 && x< cols && y >=0 && y< rows){
-    if (grid[y][x] ===1){
-      grid[y][x] = 0;
+    if (lines[y][x] === 1){
+      lines[y][x] = 0;
     }
 
-    else if (grid[y][x] === 0){
-      grid[y][x] = 1;
+    else if (lines[y][x] === 0){
+      lines[y][x] = 1;
     }
   }
 }
 
-function generateRandomGrid(cols, rows){
-  let newGrid = [];
+function toggleLinesY(x, y){
+  if (x>=0 && x< cols && y >=0 && y< rows){
+    if (lines[y][x] === 2){
+      lines[y][x] = 0;
+    }
 
-  for(let y = 0; y < rows; y ++){
-    newGrid.push([]);
-    for(let x = 0; x < cols; x++){
-      if(random(100) < 50){
-        newGrid[y].push(1);
-      }
-      else {
-        newGrid[y].push(0);
-      }
+    else if (lines[y][x] === 0){
+      lines[y][x] = 2;
     }
   }
-  return newGrid;
 }
+
+function lineCorrect(x,y){
+  let onX = false;
+  let onY = false;
+
+  if (x % cols >= 0 - lineWidth && x % cols <= 0 + lineWidth){
+    onX = true;
+    return lineCorrect = true;
+  }
+
+  if (y % rows >= 0 - lineWidth && y % rows <= 0 + lineWidth){
+    onY = true;
+    return lineCorrect = false;
+  }
+}
+
+// function toggleGrid(x, y){
+//   if (x>=0 && x< cols && y >=0 && y< rows){
+//     if (grid[y][x] === 1){
+//       grid[y][x] = 0;
+//     }
+
+//     else if (grid[y][x] === 0){
+//       grid[y][x] = 1;
+//     }
+//   }
+// }
+
+// function generateRandomGrid(cols, rows){
+//   let newGrid = [];
+
+//   for(let y = 0; y < rows; y ++){
+//     newGrid.push([]);
+//     for(let x = 0; x < cols; x++){
+//       if(random(100) < 50){
+//         newGrid[y].push(1);
+//       }
+//       else {
+//         newGrid[y].push(0);
+//       }
+//     }
+//   }
+//   return newGrid;
+// }
 
 function keyPressed(){
   if (key === "r"){
@@ -95,7 +150,7 @@ function keyPressed(){
   }
 }
 
-function generateEmptyGrid(cols, rows){
+function generateGrid(cols, rows){
   let newGrid = [];
 
   for(let y = 0; y < rows; y ++){
@@ -105,4 +160,16 @@ function generateEmptyGrid(cols, rows){
     }
   }
   return newGrid;
+}
+
+function generateLines(cols, rows){
+  let newLines = [];
+
+  for(let y = 0; y < rows; y ++){
+    newLines.push([]);
+    for(let x = 0; x < cols; x++){
+      newLines[y].push(0);
+    }
+  }
+  return newLines;
 }

f58bd3360d12f7c1b17b7acc7953add2eaa96c7f
audreyd14 <ardeschamp@gmail.com>
2026-04-02 10:23:54 -0600

    fixing line nesw

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -14,7 +14,6 @@ let rows;
 let cols;
 let lineWidth = 10;
 let horizontal = true;
-let startLines;
 let allLines = [];
 
 
@@ -35,7 +34,7 @@ function draw() {
 function displayGrid(){
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
-      if (grid[y][x] === 0){
+      if (rectGrid[y][x] === 0){
         fill("white");
       }
       rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
@@ -45,110 +44,83 @@ function displayGrid(){
 }
 
 function displayLines(){
+  for (let i of allLines){
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
-      showLineN(lines[y][x], x, y);
-      showLineE(lines[y][x], x, y);
-      showLineS(lines[y][x], x, y);
-      showLineW(lines[y][x], x, y);
+      showLineN(i.n, x, y);
+      showLineE(i.e, x, y);
+      showLineS(i.s, x, y);
+      showLineW(i.w, x, y);
     }
   }
+}
 }
 
 
 //NORTH
-function showLineN(line.n, x, y){
-  if (line.n === 0){
+function showLineN(){
+  if (i.n === 0){
     stroke(200, 200, 200);
     strokeWeight(lineWidth);
     line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH);
-    line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
   }
-  if (line.n === 1){
+  if (i.n === 1){
     stroke(200, 0, 0);
     strokeWeight(lineWidth);
     line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH);
   }
 }
 //EAST
-function showLineE(line.e, x, y){
-  if (line.e === 0){
+function showLineE(){
+  if (i.e === 0){
     stroke(200, 200, 200);
     strokeWeight(lineWidth);
-    line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH);
     line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
   }
-  if (line.e === 1){
+  if (i.e === 1){
     stroke(200, 0, 0);
     strokeWeight(lineWidth);
     line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
   }
 }
 //SOUTH
-function showLineS(line.s, x, y){
-  if (line.s === 0){
+function showLineS(){
+  if (i.s === 0){
     stroke(200, 200, 200);
     strokeWeight(lineWidth);
-    line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH);
-    line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
+    line(x * LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
   }
-  if (line.s === 1){
+  if (i.s === 1){
     stroke(200, 0, 0);
     strokeWeight(lineWidth);
-    line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH);
+    line(x * LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
   }
 }
 //WEST
-function showLineW(line.w, x, y){
-  if (line.w === 0){
+function showLineW(){
+  if (i.w === 0){
     stroke(200, 200, 200);
     strokeWeight(lineWidth);
-    line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH);
-    line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
+    line(x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
   }
-  if (line.w === 1){
+  if (i.w === 1){
     stroke(200, 0, 0);
     strokeWeight(lineWidth);
-    line(x * LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
+    line(x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH, x * LINE_LENGTH + LINE_LENGTH, y * LINE_LENGTH + LINE_LENGTH);
   }
 }
 
-function mousePressed(){
-  let x = Math.floor((mouseX*LINE_LENGTH)/ cols);
-  let y = Math.floor((mouseY*LINE_LENGTH)/ rows);
-  lineCorrect(x, y);
-  if (horizontal === true){
-    toggleLinesX(x, y);
-  }
-  if (horizontal === false){
-    toggleLinesY(x, y);
-  }
-}
+function mousePressed(){////////////////
+  let x = Math.floor(mouseX/LINE_LENGTH);
+  let y = Math.floor(mouseY/LINE_LENGTH);
 
-function toggleLinesX(x, y){
-  if (x>=0 && x< cols && y >=0 && y< rows){
-    if (lines[y][x] === 1){
-      lines[y][x] = 0;
-    }
-
-    else if (lines[y][x] === 0){
-      lines[y][x] = 1;
-    }
-  }
+  toggleLinesX(x,y);
 }
 
 
 
-function lineCorrect(x,y){
 
-  if (mouseY <= y + lineWidth || mouseY >= y - lineWidth && mouseY <= y + lineWidth){
-    horizontal = true;
-  }
 
-  else if (mouseX <= x + lineWidth || mouseX >= x - lineWidth){
-    horizontal = false;
-  }
-}
 
 // function lineCorrect(x,y){
 
@@ -205,13 +177,32 @@ function generateGrid(cols, rows){
 }
 
 function generateLines(cols, rows){
+  let north;
+  let east;
+  let south;
+  let west;
+  //NORTH
+  if( mouseY <= rows*LINE_LENGTH + lineWidth && mouseY >= rows*LINE_LENGTH - lineWidth){
+    north = 1;
+  }
+  //EAST
+  if( mouseX <= cols*LINE_LENGTH + lineWidth && mouseX >= cols*LINE_LENGTH - lineWidth){
+    east = 1;
+  }
+  //SOUTH
+  if( mouseY <= rows*LINE_LENGTH + LINE_LENGTH + lineWidth && mouseY >= rows*LINE_LENGTH +LINE_LENGTH - lineWidth){
+    south = 1;
+  }
+  //WEST
+  if( mouseX <= cols*LINE_LENGTH + LINE_LENGTH + lineWidth && mouseX >= cols*LINE_LENGTH + LINE_LENGTH - lineWidth){
+    west = 1;
+  }
 
-  if( mouseY <= row*LINE_LENGTH + lineWidth && )
   let lineCoords = [
-    n: North,
-    e: East,
-    w: West,
-    s: South,
+    n: north,
+    e: east,
+    w: west,
+    s: south,
   ];
   
 
